24d90023ed87a451e0be51df2873d6b3e2698fd2
Ahmed Alaa <medo.oreif@gmail.com>
2026-05-22 21:25:54 +0300

    data validation notebook added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
+from textSummarizer.pipeline.stage_02_data_validation import DatavalidationTrainingPipeline
 from textSummarizer.logging import logger
 
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = "Data validation Stage"
 try:
     logger.info(f"------------- stage {STAGE_NAME} started -----------------------")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
+    data_validation = DatavalidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"------------- stage {STAGE_NAME} completed ---------------------")
 
 except Exception as e:
